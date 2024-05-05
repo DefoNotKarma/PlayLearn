5ce99476b6ba7ec605e0327806d64946eebdceae
DefoNotKarma <utkarshg74@gmail.com>
2024-05-06 00:29:33 +0530

    Revert "added terrain and setup player"
    This reverts commit 51e9cf46ef118cbf5028d97908baef42a47b74de.

--- GameAssets/PlayLearn/Assets/Assets/Scripts/FollowMouse.cs
+++ GameAssets/PlayLearn/Assets/Assets/Scripts/FollowMouse.cs
@@ -6,30 +6,27 @@ public class FollowMouse : MonoBehaviour
 {
 
     public float speed = 5f;
-
+    private Transform m_transform;
+    Vector2 Cursor = 
+    Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    
     // Start is called before the first frame update
     void Start()
     {
-
+        m_transform = this.transform;
     }
 
+    private void lookAtMouse(){
+        Vector2 direction = Cursor - m_transform.position;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 Cursor = 
-        Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        Vector2 direction = new Vector2(
-            Cursor.x - transform.position.x,
-            Cursor.y - transform.position.y
-        );
-
-        transform.up = direction;
+        lookAtMouse()
     
         transform.position = 
-        Vector2.MoveTowards(transform.position,
-         Cursor, speed *  Time.deltaTime);
-     
+        Vector2.MoveTowards(transform.position, Cursor, speed *  Time.deltaTime);
+        
     }
 }
